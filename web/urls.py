421514f79320f010fe69ec6bdec38488be1ee820
ardu1na <arduinadelbosque@gmail.com>
2023-04-21 15:04:49 -0300

    add portfolio detail two pages

--- web/urls.py
+++ web/urls.py
@@ -4,4 +4,8 @@ from web import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('servicios/web/', views.servicios_web, name="servicios_web"),
+    path('servicios/iching/', views.servicios_iching, name="servicios_iching"), 
+
+    
 ]
--- web/views.py
+++ web/views.py
@@ -4,3 +4,15 @@ def index(request):
     template_name = "index.html"
     context = {}
     return render(request, template_name, context)
+
+
+def servicios_web(request):
+    template_name = "servicios/web.html"
+    context = {}
+    return render(request, template_name, context)
+
+
+def servicios_iching(request):
+    template_name = "servicios/iching.html"
+    context = {}
+    return render(request, template_name, context)
